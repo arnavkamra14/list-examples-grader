e05308dfb3fcfc42fbcba2991f8e309689c1881a
arnavkamra14 <arnavkamra14@gmail.com>
2022-11-27 23:58:54 -0800

    Update TestListExamples.java

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,52 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class TestListExamples {
-  // Write your grading tests here!
+  @Test
+  public void mergeTest() {
+
+    List<String> input1 = new ArrayList<>();
+    input1.add("Apple");
+    input1.add("Bat");
+    input1.add("Firefighter");
+
+    List<String> input2 = new ArrayList<>();
+    input2.add("Cat");
+    input2.add("Dog");
+    input2.add("Elephant");
+
+    List<String> expected = new ArrayList<>();
+    expected.add("Apple");
+    expected.add("Bat");
+    expected.add("Cat");
+    expected.add("Dog");
+    expected.add("Elephant");
+    expected.add("Firefighter");
+
+    assertArrayEquals(expected.toArray(), (ListExamples.merge(input1, input2)).toArray());
+  }
+
+  @Test
+  public void filterTest() {
+
+    public class IsHello implements StringChecker() {
+      public boolean checkString(String s) {
+        return (s.contains("Hello"));
+      }
+    }
+
+    List<String> input = new ArrayList<>();
+    input.add("Hello");
+    input.add("my");
+    input.add("name");
+    input.add("is");
+
+    input1.add("Hello")
+
+
+
+
+  }
 }
