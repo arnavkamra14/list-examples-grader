12557e2908de5883cfae57d9f778d8d2b237026f
arnavkamra14 <arnavkamra14@gmail.com>
2022-11-27 23:57:59 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -5,3 +5,17 @@ set -e
 rm -rf student-submission
 git clone $1 student-submission
 
+cd student-submission
+
+if [ -e "ListExamples.java" ];
+then
+    cd ..
+    cp ./student-submission/ListExamples.java ListExamples.java
+else    
+    echo "The ListExamples.java file does not exist in the submission"
+fi
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar TestListExamples.java TestListExamples.java; [ $? -eq 0 ] || echo "The file did not compile successfully"
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+
+
